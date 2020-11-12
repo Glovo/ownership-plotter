571877b2e179980512b7c6eb10eecd3566213f92
Paweł Szymański <pawelfiu@gmail.com>
2020-11-12 09:54:12 +0100

    Don't draw arrows to methods owned by same owner

--- src/main/java/com/glovoapp/ownership/plotting/plantuml/PlantUMLDiagramDataTransformer.java
+++ src/main/java/com/glovoapp/ownership/plotting/plantuml/PlantUMLDiagramDataTransformer.java
@@ -2,7 +2,6 @@ package com.glovoapp.ownership.plotting.plantuml;
 
 import static com.glovoapp.ownership.plotting.plantuml.Arrow.Attribute.BOLD;
 import static com.glovoapp.ownership.plotting.plantuml.Utils.RANDOM;
-import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
@@ -16,10 +15,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.plantuml.SourceStringReader;
 
@@ -31,7 +30,7 @@ public final class PlantUMLDiagramDataTransformer implements DiagramDataTransfor
 
     /**
      * @param diagramConfiguration global diagram settings
-     * @param ownershipFilters only classes that match any of given filters will be drawn on the diagram
+     * @param ownershipFilters     only classes that match any of given filters will be drawn on the diagram
      */
     public PlantUMLDiagramDataTransformer(final DiagramConfiguration diagramConfiguration,
                                           final Collection<OwnershipFilter> ownershipFilters) {
@@ -84,7 +83,14 @@ public final class PlantUMLDiagramDataTransformer implements DiagramDataTransfor
                                       .distinct()
                                       .map(uniqueOwnerName ->
                                           owners.stream()
-                                                .filter(owner -> uniqueOwnerName.equals(owner.getName()))
+                                                .filter(owner ->
+                                                    !Objects.equals(
+                                                        owner.getName(),
+                                                        component.getClassOwnership()
+                                                                 .getClassOwner()
+                                                    )
+                                                )
+                                                .filter(owner -> Objects.equals(uniqueOwnerName, owner.getName()))
                                                 .findAny()
                                       )
                                       .filter(Optional::isPresent)
