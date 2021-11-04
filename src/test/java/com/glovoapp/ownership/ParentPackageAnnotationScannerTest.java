39b6a7dcde406fe963dc389eb0804732500903a7
carmine <carmine.ingaldi@glovoapp.com>
2021-11-04 08:56:18 +0100

    remove nipple references

--- src/test/java/com/glovoapp/ownership/ParentPackageAnnotationScannerTest.java
+++ src/test/java/com/glovoapp/ownership/ParentPackageAnnotationScannerTest.java
@@ -13,47 +13,47 @@ class ParentPackageAnnotationScannerTest {
     @Test
     void getPackageName_givenClass_shouldReturnPackageName() {
 
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertEquals("com.glovoapp.ownership.examples.parentpackage.childpackage", sut.getPackageName(NotAnnotatedClass.class).get());
+        assertEquals("com.glovoapp.ownership.examples.parentpackage.childpackage", scanner.getPackageName(NotAnnotatedClass.class).get());
     }
 
 
     @Test
     void getSuperPackageName_givenOneLevel_shouldReturnEmpty() {
 
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertEquals("", sut.getSuperPackageName("com"));
+        assertEquals("", scanner.getSuperPackageName("com"));
     }
 
     @Test
     void getSuperPackageName_givenTwoLevels_shouldReturnFirstLevel() {
 
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertEquals("com", sut.getSuperPackageName("com.glovoapp"));
+        assertEquals("com", scanner.getSuperPackageName("com.glovoapp"));
     }
 
     @Test
     void getSuperPackageName_givenThreeLevels_shouldReturnSecond() {
 
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertEquals("com.glovoapp", sut.getSuperPackageName("com.glovoapp.ownership"));
+        assertEquals("com.glovoapp", scanner.getSuperPackageName("com.glovoapp.ownership"));
     }
 
     @Test
     void scan_givenClass_shouldFindParentPackageAnnotation() {
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertNotNull(sut.scan(NotAnnotatedClass.class).get());
+        assertNotNull(scanner.scan(NotAnnotatedClass.class).get());
     }
 
     @Test
     void scan_givenNotAnnotatedPackage_shouldReturnNull() {
-        var sut = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
+        var scanner = new ParentPackageAnnotationScanner(ExampleOwnershipAnnotation.class);
 
-        assertFalse(sut.scan(ClassInNotAnnotatedPackage.class).isPresent());
+        assertFalse(scanner.scan(ClassInNotAnnotatedPackage.class).isPresent());
     }
 }
