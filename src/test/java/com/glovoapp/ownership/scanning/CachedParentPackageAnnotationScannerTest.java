446722459f80e4233c143653070bdc070a663560
carmine <carmine.ingaldi@glovoapp.com>
2021-11-09 19:26:52 +0100

    minor

--- src/test/java/com/glovoapp/ownership/scanning/CachedParentPackageAnnotationScannerTest.java
+++ src/test/java/com/glovoapp/ownership/scanning/CachedParentPackageAnnotationScannerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class CachedParentPackageAnnotationScannerTest {
