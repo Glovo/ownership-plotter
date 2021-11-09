9f257c241dd692d475374ecd80830f97209538d2
carmine <carmine.ingaldi@glovoapp.com>
2021-11-09 19:26:34 +0100

    test: add test

--- src/test/java/com/glovoapp/ownership/scanning/CachedParentPackageAnnotationScannerTest.java
+++ src/test/java/com/glovoapp/ownership/scanning/CachedParentPackageAnnotationScannerTest.java
@@ -1,6 +1,7 @@
 package com.glovoapp.ownership.scanning;
 
 import com.glovoapp.ownership.examples.ExampleOwnershipAnnotation;
+import com.glovoapp.ownership.examples.packageA.ApiA;
 import com.glovoapp.ownership.examples.parentpackage.childpackage.AnotherNotAnnotatedClass;
 import com.glovoapp.ownership.examples.parentpackage.childpackage.NotAnnotatedClass;
 import lombok.var;
@@ -25,6 +26,20 @@ class CachedParentPackageAnnotationScannerTest {
         scanner.scan(AnotherNotAnnotatedClass.class);
 
         verify(delegate, times(1)).scan((any()));
+
+    }
+
+
+    @Test
+    void givenClassesFromDifferentPackages_whenScan_shouldInvokeDelegateTwice() {
+
+        when(delegate.scan(any())).thenReturn(Optional.empty());
+        var scanner = new CachedParentPackageAnnotationScanner(delegate);
+
+        scanner.scan(NotAnnotatedClass.class);
+        scanner.scan(ApiA.class);
+
+        verify(delegate, times(2)).scan((any()));
 
     }
 }
